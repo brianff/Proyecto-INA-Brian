942586006842d47e9dc69c85708f56c8fa3dc07f
brianff <31262812+brianff@users.noreply.github.com>
2017-08-22 19:49:00 -0600

    asd

--- AccesoDatos/clsAccesoDatosDeduccion.vb
+++ AccesoDatos/clsAccesoDatosDeduccion.vb
@@ -103,6 +103,73 @@ Namespace AccesoDatos
             End Try
             Return vln_IdResultado
         End Function
+        ''Condicion para ver el parametro que se va a buscar
+        ''Orden para ordenarlos
+        Public Function ListarRegistros(ByVal pvc_Condicion As String, ByVal pvc_Orden As String) As DataSet
+            Dim vlo_SqlConnection As New SqlConnection(vgc_CadenaConexion)
+            Dim vlo_SqlDataAdapter As SqlDataAdapter
+            Dim vlo_dsDatos As New DataSet
+            Dim vlc_Sentencia As String = String.Empty
+
+            vlc_Sentencia = "SELECT Cod_Deduccion, Descripcion, Monto, Tipo, ValorDeduccio1, ValorDeduccion2 FROM Deducciones"
+
+            If Not String.IsNullOrEmpty(pvc_Condicion) Then
+                '' el 0 representa la vlc_Sentencia y el 1 la pvc_Condicion
+                vlc_Sentencia = String.Format("{0} WHERE {1}", vlc_Sentencia, pvc_Condicion)
+            End If
+            If Not String.IsNullOrEmpty(pvc_Orden) Then
+                vlc_Sentencia = String.Format("{0} ORDER BY {1}", vlc_Sentencia, pvc_Orden)
+            End If
+            Try
+                vlo_SqlDataAdapter = New SqlDataAdapter(vlc_Sentencia, vlo_SqlConnection)
+                vlo_SqlDataAdapter.Fill(vlo_dsDatos, "Deducciones")
+            Catch ex As Exception
+                Throw
+            Finally
+                vlo_SqlConnection.Dispose()
+            End Try
+            Return vlo_dsDatos
+        End Function
+        ''cuando la sentencia es un select hay que usarlo con executedatareader
+        Public Function ObtenerRegistro(ByVal pvc_Condicion As String) As clsDeducciones
+            Dim vlo_SqlConnection As New SqlConnection(vgc_CadenaConexion)
+            Dim vlo_SqlCommand As New SqlCommand
+            Dim vlo_SqlDataReader As SqlDataReader
+            Dim vlc_Sentencia As String = String.Empty
+            Dim vlo_EntidadPago As New clsDeducciones
+
+            vlc_Sentencia = "SELECT Cod_Deduccion, Descripcion, Monto, Tipo, ValorDeduccion1, ValorDeduccion2 FROM Deducciones"
+
+            If Not String.IsNullOrEmpty(pvc_Condicion) Then
+                vlc_Sentencia = String.Format("{0} WHERE {1}", vlc_Sentencia, pvc_Condicion)
+            End If
+            Try
+                vlo_SqlCommand.Connection = vlo_SqlConnection
+                vlo_SqlConnection.Open()
+                vlo_SqlCommand.CommandText = vlc_Sentencia
+
+                vlo_SqlDataReader = vlo_SqlCommand.ExecuteReader()
+                If vlo_SqlDataReader.HasRows Then
+                    vlo_SqlDataReader.Read()
+                    vlo_EntidadPago.ExisteRegistro = True
+                    vlo_EntidadPago.Cod_Deduccion = CType(vlo_SqlDataReader("Cod_Deduccion"), Integer)
+                    vlo_EntidadPago.Descripcion = CType(vlo_SqlDataReader("Descripcion"), String)
+                    vlo_EntidadPago.Monto = CType(vlo_SqlDataReader("Monto"), Double)
+                    vlo_EntidadPago.Tipo = CType(vlo_SqlDataReader("Tipo"), Integer)
+                    vlo_EntidadPago.ValorDeduccion1 = CType(vlo_SqlDataReader("ValorDeduccion1"), Double)
+                    vlo_EntidadPago.ValorDeduccion2 = CType(vlo_SqlDataReader("ValorDeduccion2"), Double)
+
+                End If
+                vlo_SqlConnection.Close()
+
+            Catch ex As Exception
+                Throw
+            Finally
+                vlo_SqlCommand.Dispose()
+                vlo_SqlConnection.Dispose()
+            End Try
+            Return vlo_EntidadPago
+        End Function
 #End Region
     End Class
 End Namespace
